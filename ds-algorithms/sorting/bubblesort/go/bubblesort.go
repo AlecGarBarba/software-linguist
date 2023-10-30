f163075b59f4124ea15c202a9feee451eac0eeb9
Alec García Barba <38474080+AlecGarBarba@users.noreply.github.com>
2023-10-29 23:19:01 -0600

    refactor deref

--- ds-algorithms/sorting/bubblesort/go/bubblesort.go
+++ ds-algorithms/sorting/bubblesort/go/bubblesort.go
@@ -14,12 +14,12 @@ func Descending(a int, b int) bool {
 * Simple Bubblesort algorithm, takes an array of integers,
 * returns it ordered based on an orderFunction.
  */
-func BubbleSort(arr *[]int, should_order OrderFunction) {
-
-	for i := 1; i < len(*arr); i++ {
-		for j := 0; j < len(*arr)-i; j++ {
-			if should_order((*arr)[j], (*arr)[j+1]) {
-				(*arr)[j], (*arr)[j+1] = (*arr)[j+1], (*arr)[j]
+func BubbleSort(r_arr *[]int, should_order OrderFunction) {
+	arr := *r_arr
+	for i := 1; i < len(arr); i++ {
+		for j := 0; j < len(arr)-i; j++ {
+			if should_order(arr[j], arr[j+1]) {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
 			}
 		}
 	}
