aa77cec9b65f365870865946a19012c80e071282
Alec García Barba <38474080+AlecGarBarba@users.noreply.github.com>
2023-11-05 20:08:15 -0600

    Get, Append and Prepend tests

--- data-structures/linked-list/go/linked-list._test.go
+++ data-structures/linked-list/go/linked-list._test.go
@@ -12,13 +12,29 @@ TESTING SINGLY LINKED LIST
 
 func TestSinglyLInkedListAppend(t *testing.T) {
 	values := []int{1, 2, 3, 4, 5, 6}
-
 	sll := SingleLinkedList[int]{}
-
-	for value := range values {
+	for index, value := range values {
 		sll.Append(value)
+		ll_value, err := sll.Get(index)
+		if err != nil {
+			t.Errorf("Error appending and getting last value")
+		}
+		assert.Equal(t, value, *ll_value)
 	}
-
 	assert.Equal(t, sll.Length(), uint(len(values)))
 
+}
+
+func TestSinglyLInkedListPrepend(t *testing.T) {
+	values := []int{1, 2, 3, 4, 5, 6}
+	sll := SingleLinkedList[int]{}
+	for value := range values {
+		sll.Prepend(value)
+		head, err := sll.Get(0)
+		if err != nil {
+			t.Errorf("Error prepending and getting first ealement")
+		}
+		assert.Equal(t, *head, value)
+	}
+	assert.Equal(t, sll.Length(), uint(len(values)))
 }
--- data-structures/linked-list/go/linked-list.go
+++ data-structures/linked-list/go/linked-list.go
@@ -1,15 +1,14 @@
 package linked_list
 
-/*
-*
- */
+import "fmt"
+
 type ILinkedList[T interface{}] interface {
 	Length() uint
-	InsertAt(item T, index uint)
-	RemoveAt(index uint) (T, error)
+	InsertAt(item T, index int)
+	RemoveAt(index int) (T, error)
 	Append(item T)
 	Prepend(item T)
-	Get(index uint) (T, error)
+	Get(index int) (T, error)
 }
 
 type Node[T interface{}] struct {
@@ -28,7 +27,7 @@ func (ll *SingleLinkedList[T]) Length() uint {
 
 func (ll *SingleLinkedList[T]) Append(item T) {
 	newNode := &Node[T]{value: item}
-	ll.len = ll.len + 1
+	ll.len++
 	if ll.head == nil {
 		ll.head = newNode
 		return
@@ -40,4 +39,30 @@ func (ll *SingleLinkedList[T]) Append(item T) {
 	}
 
 	curr.next = newNode
+}
+
+func (ll *SingleLinkedList[T]) Prepend(item T) {
+	newNode := &Node[T]{value: item}
+	ll.len++
+
+	if ll.head == nil {
+		ll.head = newNode
+		return
+	}
+
+	newNode.next = ll.head
+	ll.head = newNode
+}
+
+func (ll *SingleLinkedList[T]) Get(index int) (*T, error) {
+	curr := ll.head
+
+	for i := 0; i < index; i++ {
+		if curr == nil {
+			return nil, fmt.Errorf("Value not found")
+		}
+		curr = curr.next
+	}
+
+	return &curr.value, nil
 }
