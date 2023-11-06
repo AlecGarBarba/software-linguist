a6ac912f6f8ac92c3167696851035b2954cdd38b
Alec García Barba <38474080+AlecGarBarba@users.noreply.github.com>
2023-11-06 00:35:08 -0600

    finish insertAt and removeAt

--- data-structures/linked-list/go/linked-list._test.go
+++ data-structures/linked-list/go/linked-list._test.go
@@ -38,3 +38,60 @@ func TestSinglyLInkedListPrepend(t *testing.T) {
 	}
 	assert.Equal(t, sll.Length(), uint(len(values)))
 }
+
+func TestSinglyLinkedListInsertAt(t *testing.T) {
+
+	values := []int{1, 2, 3, 4, 5}
+	sll := SingleLinkedList[int]{}
+
+	for value := range values {
+		sll.Append(value)
+	}
+	/*insert out of index range*/
+
+	err := sll.InsertAt(6, len(values)+5)
+
+	assert.Error(t, err, "Error expected as index to insert is out of range")
+	/*insert beginning*/
+	sll.InsertAt(0, 0)
+	head, _ := sll.Get(0)
+	assert.Equal(t, *head, 0)
+
+	/*insert at the end*/
+
+	end_err := sll.InsertAt(6, 6)
+	if end_err != nil {
+		t.Errorf("Error inserting at end of linked list, reason: %v", end_err)
+	}
+	tail, _ := sll.Get(6)
+
+	assert.Equal(t, *tail, 6)
+}
+
+func TestSinglyLinkedList(t *testing.T) {
+	values := []int{1, 2, 3, 4, 5}
+
+	sll := SingleLinkedList[int]{}
+	for _, value := range values {
+		sll.Append(value)
+	}
+
+	/*remove at the end*/
+	end_val, end_err := sll.RemoveAt(4)
+	assert.Nil(t, end_err, "Should properly remove last index")
+	assert.Equal(t, 5, *end_val)
+	/*remove at beggining*/
+	beg_val, beg_err := sll.RemoveAt(0)
+	/*Remove until list is empty*/
+	assert.Nil(t, beg_err, "Should properly remove at beggining")
+	assert.Equal(t, values[0], *beg_val)
+
+	for i := 0; i < int(sll.Length()); i++ {
+		val, err := sll.RemoveAt(0)
+
+		assert.Nil(t, err, "Remove out of range")
+
+		assert.NotNil(t, val)
+
+	}
+}
--- data-structures/linked-list/go/linked-list.go
+++ data-structures/linked-list/go/linked-list.go
@@ -1,10 +1,12 @@
 package linked_list
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type ILinkedList[T interface{}] interface {
 	Length() uint
-	InsertAt(item T, index int)
+	InsertAt(item T, index int) error
 	RemoveAt(index int) (T, error)
 	Append(item T)
 	Prepend(item T)
@@ -65,4 +67,65 @@ func (ll *SingleLinkedList[T]) Get(index int) (*T, error) {
 	}
 
 	return &curr.value, nil
+}
+
+func (ll *SingleLinkedList[T]) InsertAt(item T, index int) error {
+	if ll.Length() == 0 {
+		ll.head = &Node[T]{value: item}
+		ll.len++
+		return nil
+	}
+	curr := ll.head
+	for i := 1; i < index; i++ {
+		if curr == nil {
+			return fmt.Errorf("Cannot insert at index %v since it's larged than current length %v", index, ll.len)
+		}
+		curr = curr.next
+	}
+	new := &Node[T]{value: item, next: curr.next}
+	curr.next = new
+	ll.len++
+	return nil
+}
+
+func (ll *SingleLinkedList[T]) RemoveAt(index int) (*T, error) {
+	if index > int(ll.len) {
+		return nil, fmt.Errorf("Index out of range")
+	}
+
+	if index == 0 && ll.Length() == 1 {
+		val := ll.head.value
+		ll.head = nil
+		ll.len = 0
+		return &val, nil
+	}
+
+	if index == 0 {
+		val := ll.head.value
+		ll.head = ll.head.next
+		ll.len--
+		return &val, nil
+	}
+
+	curr := ll.head
+
+	for i := 0; i < index-1; i++ {
+		curr = curr.next
+	}
+
+	value := curr.next.value
+	curr.next = curr.next.next
+	ll.len--
+	return &value, nil
+
+}
+
+func (ll *SingleLinkedList[T]) PrintAll() {
+	curr := ll.head
+	for curr != nil {
+		fmt.Printf("%v, ", curr.value)
+		curr = curr.next
+	}
+	fmt.Println()
+
 }
